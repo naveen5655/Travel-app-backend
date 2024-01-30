fab418d028ee3eee06877962b4de8314b0d575bd
Badisa Naveen <naveenbadisa5655@gmail.com>
2024-01-30 22:40:06 +0530

    Add APIs

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 
+
+const hotelRouter = require("./routes/hotel.router");
+
 const app = express();
+
+app.use(express.json());
 
 const PORT = 3500;
 
 app.get("/",(req,res)=>{
     res.send("Hello Badisa Naveen")
 })
+
+app.use("/api/hotels",hotelRouter);
 
 app.listen(process.env.PORT || PORT ,()=>{
     console.log("Server is UP and Running")
