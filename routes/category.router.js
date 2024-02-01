4dd85aa04bf6f26e7106f152dd5ed4d24a06fe31
Badisa Naveen <naveenbadisa5655@gmail.com>
2024-02-01 07:46:49 +0530

    Added APi

---
+++ routes/category.router.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const Category = require("../model/category.model");
+
+router.route("/")
+    .get(async(req,res)=>{
+        try {
+            const categories = await Category.find({});
+            res.json(categories)
+        }
+        catch(err)
+        {
+            res.status(404).json({message:"Could not find category"})
+        }
+    })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const mongoose = require('mongoose');
 
 const hotelDataAddedToDBRouter = require("./routes/dataimport.router");
+const categoryDataAddedToDBRouter = require("./routes/categoryimport.router");
 
 const hotelRouter = require("./routes/hotel.router");
+const categoryRouter = require("./routes/category.router")
 const connectDB = require("./config/dbconfig");
 
 const app = express();
@@ -18,8 +20,9 @@ app.get("/",(req,res)=>{
 })
 
 app.use("/api/hoteldata", hotelDataAddedToDBRouter);
-
+app.use("/api/categorydata", categoryDataAddedToDBRouter);
 app.use("/api/hotels",hotelRouter);
+app.use("/api/category",categoryRouter);
 
 mongoose.connection.once("open", () =>{
     console.log("Connected to db");
